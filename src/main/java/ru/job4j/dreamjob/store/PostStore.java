d7ed6666418857f3b29efdfd6ec1fa36c8f192e1
IvanPavlovets <ivan150287@gmail.com>
2022-04-14 16:03:07 +0700

    4. Thymeleaf, Циклы. [#504841]

---
+++ src/main/java/ru/job4j/dreamjob/store/PostStore.java
@@ -0,0 +1,45 @@
+package ru.job4j.dreamjob.store;
+
+import ru.job4j.dreamjob.model.Post;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Collection;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * Модель данных.
+ * Класс хранилище вакансий.
+ */
+public class PostStore {
+    private static final PostStore INST = new PostStore();
+
+    /**
+     * Внутреняя карта для работы с многопоточностью.
+     */
+    private final Map<Integer, Post> posts = new ConcurrentHashMap<>();
+
+    public PostStore() {
+        String date = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
+        posts.put(1, new Post(1, "Junior Java Job", "desc Junior", date));
+        posts.put(2, new Post(2, "Middle Java Job", "desc Middle", date));
+        posts.put(3, new Post(3, "Senior Java Job", "desc Senior", date));
+    }
+
+    /**
+     * Геттер экземпляра класса.
+     * @return PostStore
+     */
+    public static PostStore instOf() {
+        return INST;
+    }
+
+    /**
+     * Геттер сех значений хранилища
+     * @return Collection<Post>
+     */
+    public Collection<Post> findAll() {
+        return posts.values();
+    }
+}
