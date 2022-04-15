1063dd483fdbc723412e31ee359a39c401296013
IvanPavlovets <ivan150287@gmail.com>
2022-04-15 14:09:43 +0700

    7. HTML form. Создание вакансии. [#504844] + навигация на всех страницах

--- src/main/java/ru/job4j/dreamjob/controller/PostController.java
+++ src/main/java/ru/job4j/dreamjob/controller/PostController.java
@@ -3,7 +3,11 @@ package ru.job4j.dreamjob.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import ru.job4j.dreamjob.model.Post;
 import ru.job4j.dreamjob.store.PostStore;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 /**
  * Контролер (Блок управления) обьектами Post
@@ -14,6 +18,7 @@ public class PostController {
     private final PostStore store = PostStore.instOf();
 
     /**
+     * Обрабатывает переход на posts.html
      * Используется Thymeleaf для поиска объектов,
      * которые нужны отобразить на виде.
      * @return String
@@ -22,5 +27,17 @@ public class PostController {
     public String posts(Model model) {
         model.addAttribute("posts", store.findAll());
         return "posts";
+    }
+
+    /**
+     * Обрабатывает переход на addPost.html
+     * @param model
+     * @return
+     */
+    @GetMapping("/formAddPost")
+    public String addPost(Model model) {
+        String date = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
+        model.addAttribute("post", new Post(0, "Заполните поле", "", date));
+        return "addPost";
     }
 }
