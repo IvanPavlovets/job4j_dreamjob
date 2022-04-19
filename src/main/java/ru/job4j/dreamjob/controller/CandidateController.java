fc55a7a4bc35efa80f3fbc1f0419a279fa958dcc
IvanPavlovets <ivan150287@gmail.com>
2022-04-19 17:33:35 +0700

    5. Создания и редактирования кандидатов. [#504858]

--- src/main/java/ru/job4j/dreamjob/controller/CandidateController.java
+++ src/main/java/ru/job4j/dreamjob/controller/CandidateController.java
@@ -3,6 +3,9 @@ package ru.job4j.dreamjob.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import ru.job4j.dreamjob.model.Candidate;
 import ru.job4j.dreamjob.store.CandidateStore;
 
@@ -31,12 +34,48 @@ public class CandidateController {
     /**
      * Обрабатывает переход на addCandidate.html
      * @param model
-     * @return
+     * @return String
      */
     @GetMapping("/formAddCandidate")
-    public String addPost(Model model) {
+    public String addCandidate(Model model) {
         String date = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
         model.addAttribute("candidate", new Candidate(0, "Заполните поле", "", date));
         return "addCandidate";
     }
+
+    /**
+     * Обрабатывает добавление данных в candidate
+     * и их сохранение в store.
+     * @param candidate
+     * @return String
+     */
+    @PostMapping("/createCandidate")
+    public String createCandidate(@ModelAttribute Candidate candidate) {
+        store.create(candidate);
+        return "redirect:/candidates";
+    }
+
+    /**
+     * Обрабатывает переход на updateCandidate.html
+     * @param model
+     * @param id
+     * @return String
+     */
+    @GetMapping("/formUpdateCandidate/{candidateId}")
+    public String formUpdateCandidate(Model model, @PathVariable("candidateId") int id) {
+        model.addAttribute("candidate", store.findById(id));
+        return "updateCandidate";
+    }
+
+    /**
+     * Сохраняет данные в candidate после редактирования.
+     * @param candidate
+     * @return String
+     */
+    @PostMapping("/updateCandidate")
+    public String updateCandidate(@ModelAttribute Candidate candidate) {
+        store.update(candidate);
+        return "redirect:/candidates";
+    }
+
 }
--- src/main/java/ru/job4j/dreamjob/store/CandidateStore.java
+++ src/main/java/ru/job4j/dreamjob/store/CandidateStore.java
@@ -7,6 +7,8 @@ import java.time.format.DateTimeFormatter;
 import java.util.Collection;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
 
 /**
  * Модель данных.
@@ -18,11 +20,15 @@ public class CandidateStore {
 
     private final Map<Integer, Candidate> candidates = new ConcurrentHashMap<>();
 
+    private static final AtomicInteger CANDIDATE_ID = new AtomicInteger(4);
+
+    private AtomicReference<String> date = new AtomicReference<>(LocalDateTime.now()
+            .format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")));
+
     public CandidateStore() {
-        String date = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
-        candidates.put(1, new Candidate(1, "Carey Mahoney", "description of Mahoney", date));
-        candidates.put(2, new Candidate(2, "Moses Hightower", "description of Hightower", date));
-        candidates.put(3, new Candidate(3, "Eugene Tackleberry", "description of Tackleberry", date));
+        candidates.put(1, new Candidate(1, "Carey Mahoney", "description of Mahoney", date.get()));
+        candidates.put(2, new Candidate(2, "Moses Hightower", "description of Hightower", date.get()));
+        candidates.put(3, new Candidate(3, "Eugene Tackleberry", "description of Tackleberry", date.get()));
     }
 
     /**
@@ -39,5 +45,32 @@ public class CandidateStore {
      */
     public Collection<Candidate> findAll() {
         return candidates.values();
+    }
+
+    /**
+     * Метод добавляет candidate во внутрение
+     * хранилище candidates(map) по ключу.
+     * @param candidate
+     */
+    public void create(Candidate candidate) {
+        candidate.setId(CANDIDATE_ID.getAndIncrement());
+        date = new AtomicReference<>(LocalDateTime.now()
+                .format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")));
+        candidate.setCreated(date.get());
+        candidates.put(candidate.getId(), candidate);
+    }
+
+    public Candidate findById(int id) {
+        return candidates.get(id);
+    }
+
+    /**
+     * Обновляет запись CandidateStore.
+     * старая запись по id меняеться на
+     * переданый candidate.
+     * @param candidate
+     */
+    public void update(Candidate candidate) {
+        candidates.replace(candidate.getId(), candidate);
     }
 }
